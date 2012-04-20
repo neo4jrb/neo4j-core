9327b38cfa84f8946cfb5a64ec2397fd50776f77
Andreas Ronge <andreas.ronge@gmail.com>
2012-04-20 10:31:57 +0200

    Fix for HA cluster neo4j-enterprise, WrappedNode closes andreasronge/neo4j#193

--- lib/neo4j-core/database.rb
+++ lib/neo4j-core/database.rb
@@ -163,8 +163,6 @@ module Neo4j
         Neo4j.logger.info "starting Neo4j in HA mode, machine id: #{Neo4j.config['ha.server_id']} at #{Neo4j.config['ha.server']} db #{@storage_path}"
         # Modify the public base classes for the HA Node and Relationships
         # (instead of private Java::OrgNeo4jKernel::HighlyAvailableGraphDatabase::LookupNode)
-        Neo4j::Node.extend_java_class(Java::OrgNeo4jToolingWrap::WrappedNode)
-        Neo4j::Relationship.extend_java_class(Java::OrgNeo4jToolingWrap::WrappedRelationship)
         @graph = Java::OrgNeo4jKernel::HighlyAvailableGraphDatabase.new(@storage_path, Neo4j.config.to_java_map)
         @graph.register_transaction_event_handler(@event_handler)
         @lucene = @graph.index
