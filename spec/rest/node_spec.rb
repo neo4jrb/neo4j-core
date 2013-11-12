98f88b5a5989696c3cf58cc3a48bb0795f7a7156
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2013-11-12 23:35:54 +0530

    Added specs for Node.load

--- spec/rest/node_spec.rb
+++ spec/rest/node_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 module Neo4j
   describe Node::Rest, api: :rest do
-    describe "instance method" do
+    describe "class method" do
       describe "new" do
         context "using current session" do
           it "creates a node" do
@@ -10,6 +10,26 @@ module Neo4j
             expect(node.name).to eq("Ujjwal")
             expect(node.email).to eq("ujjwalthaakar@gmail.com")
           end
+        end
+
+        context "using another session" do
+          it "creates a node in that session" do
+            another_session = Session.new :rest
+            node = Node.new name: "Andreas Ronge", email: "andreas.ronge@gmail.com"
+            expect(node[:name]).to eq("Andreas Ronge")
+            expect(node[:email]).to eq("andreas.ronge@gmail.com")
+          end
+        end
+      end
+
+      describe "load" do
+        it "loads the node with the given neo id" do
+          node = Node.new name: "Steve Wozniak", email: "steve.wozniak@apple.com"
+          id = node.id
+          same_node = Node.load(id)
+          expect(node.id).to eq(same_node.id)
+          expect(node[:name]).to eq(same_node[:name])
+          expect(node[:email]).to eq(same_node[:email])
         end
       end
     end
