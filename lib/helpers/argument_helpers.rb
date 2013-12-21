1ffda2a130e2e02dd9b9998bdfeef93c10144972
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2013-12-22 02:20:17 +0530

    Documented ArgumentHelpers and Relationship modules

--- lib/helpers/argument_helpers.rb
+++ lib/helpers/argument_helpers.rb
@@ -1,5 +1,16 @@
 module Neo4j
+  # Helpers for processing method arguments
   module ArgumentHelpers
+    # Extracts a session from the array of arguments if one exists at the end
+    #
+    # == Parameters:
+    # args::
+    #   An array of arguments of any type
+    #
+    # == Returns:
+    # A session if the last argument is a valid session and pops it out of args.
+    # Otherwise it returns the current session.
+    #
     def extract_session(args)
       if args.last.is_a?(Session::Rest) || args.last.is_a?(Session::Embedded)
         args.pop
