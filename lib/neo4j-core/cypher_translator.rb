b5c67e62babe6504dfbef7cdd083e2407d2a41d6
Eugen Martin <eugeniusmartinus@gmail.com>
2014-04-08 15:26:52 +0200

    add escape sequence sanitization to cypher translator

--- lib/neo4j-core/cypher_translator.rb
+++ lib/neo4j-core/cypher_translator.rb
@@ -1,14 +1,38 @@
 module Neo4j::Core
   module CypherTranslator
     # Cypher Helper
-    def escape_value(value)
-      case value
-        when String
-          "'#{value.gsub("'", %q(\\\'))}'"
-        else
-          value
-      end
-    end
+     def escape_value(value)
+       result = case value
+         when String
+           sanitized = sanitize_escape_sequences(value)
+           "'#{escape_quotes(sanitized)}'"
+         else
+           value
+       end
+       result
+     end
+
+     # Only following escape sequence characters are allowed in Cypher:
+     #
+     # \t Tab
+     # \b Backspace
+     # \n Newline
+     # \r Carriage return
+     # \f Form feed
+     # \' Single quote
+     # \" Double quote
+     # \\ Backslash
+     #
+     # From:
+     # http://docs.neo4j.org/chunked/stable/cypher-expressions.html#_note_on_string_literals
+     SANITIZE_ESCAPED_REGEXP = /(?<!\\)\\(\\\\)*(?![futbnr'"\\])/
+     def sanitize_escape_sequences(s)
+       s.gsub SANITIZE_ESCAPED_REGEXP, ''
+     end
+
+     def escape_quotes(s)
+       s.gsub("'", %q(\\\'))
+     end
 
     # Cypher Helper
     def cypher_prop_list(props)
