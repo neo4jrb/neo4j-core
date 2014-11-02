ead2ea1398726d28a133e691a21a50a774e8e684
Brian Underwood <ml+github@semi-sentient.com>
2014-11-02 22:23:09 +0100

    Simplify the find_in_batches implementation a bit and put in some specs

--- lib/neo4j-core/query_find_in_batches.rb
+++ lib/neo4j-core/query_find_in_batches.rb
@@ -2,11 +2,10 @@ module Neo4j::Core
   module QueryFindInBatches
 
     def find_in_batches(node_var, prop_var, options = {})
-      invalid_keys = options.keys.map(&:to_sym) - [:batch_size, :returned_primary_var]
+      invalid_keys = options.keys.map(&:to_sym) - [:batch_size]
       raise ArgumentError, "Invalid keys: #{invalid_keys.join(', ')}" if not invalid_keys.empty?
 
       batch_size = options.delete(:batch_size) || 1000
-      returned_primary_var = options.delete(:returned_primary_var)
 
       query = self.reorder(node_var => prop_var).limit(batch_size)
 
@@ -14,15 +13,15 @@ module Neo4j::Core
 
       while records.any?
         records_size = records.size
-        primary_key_offset = if returned_primary_var
-                            records.last.send(returned_primary_var)
-                          else
-                            begin
-                              records.last.send(node_var).send(prop_var)
-                            rescue NoMethodError
-                              records.last.send("#{node_var}.#{prop_var}") # In case we're explicitly returning it
-                            end
-                          end
+        primary_key_offset = begin
+                               records.last.send(node_var).send(prop_var)
+                             rescue NoMethodError
+                               begin
+                                 records.last.send(node_var)[prop_var.to_sym]
+                               rescue NoMethodError
+                                 records.last.send("#{node_var}.#{prop_var}") # In case we're explicitly returning it
+                               end
+                             end
 
         yield records
 
