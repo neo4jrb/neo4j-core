c4fdc9e056a9829280706bf92d11522a868ceeea
Chris Grigg <chris@subvertallmedia.com>
2015-02-14 19:26:42 -0500

    rubocop

--- spec/neo4j-core/unit/session_unit_spec.rb
+++ spec/neo4j-core/unit/session_unit_spec.rb
@@ -4,13 +4,12 @@ describe Neo4j::Session do
   let(:session) { Neo4j::Session.new }
   let(:error) { 'not impl.' }
   it 'raises errors for methods not implemented' do
-    [lambda { session.start }, lambda { session.shutdown }, lambda { session.db_type }, lambda { session.begin_tx }].each do |l|
+    [-> { session.start }, -> { session.shutdown }, -> { session.db_type }, -> { session.begin_tx }].each do |l|
       expect { l.call }.to raise_error error
     end
 
     expect { session.query }.to raise_error 'not implemented, abstract'
     expect { session._query }.to raise_error 'not implemented'
     expect { create_session(:foo) }.to raise_error
-
   end
 end
