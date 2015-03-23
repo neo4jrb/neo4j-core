3c30d4b18774a084f1370d9205809f7713c37eea
Chris Grigg <chris@subvertallmedia.com>
2015-03-23 19:08:58 -0400

    easily check for indexes, constraints and clear all of either type

--- lib/neo4j/label.rb
+++ lib/neo4j/label.rb
@@ -61,6 +61,8 @@ module Neo4j
 
     class << self
       include Neo4j::Core::CypherTranslator
+      INDEX_PATH = '/db/data/schema/index/'
+      CONSTRAINT_PATH = '/db/data/schema/constraint/'
 
       # Returns a label of given name that can be used to specifying constraints
       # @param [Symbol,String] name the name of the label
@@ -68,6 +70,38 @@ module Neo4j
         session.create_label(name)
       end
 
+      def constraints(session = Neo4j::Session.current)
+        session.connection.get(CONSTRAINT_PATH).body
+      end
+
+      def constraint?(label_name, property, session = Neo4j::Session.current)
+        !constraints(session).select { |c| c[:label] == label_name.to_s && c[:property_keys].first == property.to_s }.empty?
+      end
+
+      def indexes(session = Neo4j::Session.current)
+        session.connection.get(INDEX_PATH).body
+      end
+
+      def index?(label_name, property, session = Neo4j::Session.current)
+        !indexes(session).select { |c| c[:label] == label_name.to_s && c[:property_keys].first == property.to_s }.empty?
+      end
+
+      def drop_all_indexes(session = Neo4j::Session.current)
+        indexes.each do |i|
+          begin
+            session._query_or_fail("DROP INDEX ON :`#{i[:label]}`(#{i[:property_keys].first})")
+          rescue Neo4j::Server::CypherResponse::ResponseError
+            # This will error on each constraint. Ignore and continue.
+            next
+          end
+        end
+      end
+
+      def drop_all_constraints(session = Neo4j::Session.current)
+        constraints.each do |c|
+          session._query_or_fail("DROP CONSTRAINT ON (n:`#{c[:label]}`) ASSERT n.`#{c[:property_keys].first}` IS UNIQUE")
+        end
+      end
 
       # @return [Enumerable<Neo4j::Node>] all nodes having given label. Nodes can be wrapped in your own model ruby classes.
       def find_all_nodes(label_name, session = Neo4j::Session.current)
--- spec/shared_examples/label.rb
+++ spec/shared_examples/label.rb
@@ -9,6 +9,8 @@ RSpec.shared_examples 'Neo4j::Label' do
     @green = Neo4j::Node.create({}, @label2)
   end
 
+  after(:all) { Neo4j::Label.drop_all_constraints  && Neo4j::Label.drop_all_indexes }
+
   describe 'Neo4j::Node' do
     describe 'add_labels' do
       it 'can add labels' do
@@ -29,7 +31,6 @@ RSpec.shared_examples 'Neo4j::Label' do
         expect(node.labels).to include(:one, :two, :three)
       end
     end
-
 
     describe 'set_label' do
       it 'replace old labels with new ones' do
@@ -129,6 +130,96 @@ RSpec.shared_examples 'Neo4j::Label' do
         result = Neo4j::Label.find_nodes(@random_label, :name, 'r')
         expect(result).to include(@red)
         expect(result.count).to eq(1)
+      end
+    end
+
+    describe 'index class methods' do
+      before(:all) do
+        label = Neo4j::Label.create(:foo)
+        label.create_index('bar')
+      end
+
+      after(:all) do
+        label = Neo4j::Label.create(:foo)
+        label.drop_index('bar')
+      end
+
+      describe 'indexes' do
+        it 'lists all known indexes' do
+          indexes = Neo4j::Label.indexes
+          selected_indexes = indexes.select { |i| i[:property_keys].include?('bar') && i[:label] == 'foo' }
+          expect(selected_indexes).not_to be_empty
+        end
+      end
+
+      describe 'index?' do
+        it 'identifies a known index' do
+          expect(Neo4j::Label.index?('foo', 'bar')).to be_truthy
+        end
+
+        it 'returns false when an index is not defined' do
+          expect(Neo4j::Label.index?('bar', 'baz')).to be_falsey
+        end
+      end
+    end
+
+    describe 'constraint class methods' do
+      before(:all) do
+        label = Neo4j::Label.create(:foo)
+        label.drop_index('foo', 'bar')
+        label.create_constraint(:bar, type: :unique) unless Neo4j::Label.constraint?(:foo, :bar)
+      end
+
+      after(:all) do
+        label = Neo4j::Label.create(:foo)
+        label.drop_constraint('bar', type: :unique)
+      end
+
+      describe 'constraints' do
+        it 'lists all known constraints' do
+          constraints = Neo4j::Label.constraints
+          selected_constraints = constraints.select { |i| i[:property_keys].include?('bar')  && i[:label] == 'foo' }
+          expect(selected_constraints).not_to be_empty
+        end
+      end
+
+      describe 'constraint?' do
+        it 'recognizes a known constraint' do
+          expect(Neo4j::Label.constraint?(:foo, :bar)).to be_truthy
+        end
+
+        it 'returns false when constraint not defined' do
+          expect(Neo4j::Label.constraint?(:bar, :foo)).to be_falsey
+        end
+      end
+    end
+
+    describe 'drop_all_indexes' do
+      it 'drops all indexes' do
+        expect(Neo4j::Session.current).to receive(:_query_or_fail).at_least(1).times.with(/DROP INDEX ON/)
+        Neo4j::Label.drop_all_indexes
+      end
+    end
+
+    describe 'drop_all_constraints' do
+      let(:label) { Neo4j::Label.create(:foo) }
+
+      before do
+        label.create_constraint(:bar, type: :unique)
+      end
+
+      after do
+        if Neo4j::Label.index?(:foo, :bar)
+          label.drop_constraint('bar', type: :unique)
+          begin
+            label.drop_index(:bar)
+          rescue; end
+        end
+      end
+
+      it 'drops all constraints' do
+        expect(Neo4j::Session.current).to receive(:_query_or_fail).at_least(1).times.with(/DROP CONSTRAINT ON/)
+        Neo4j::Label.drop_all_constraints
       end
     end
   end
