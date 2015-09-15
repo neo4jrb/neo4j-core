47b15d4e41e3fdd931433f6ce4fcb2fdf0b37c79
Brian Underwood <ml+github@semi-sentient.com>
2015-09-16 09:29:58 +1200

    Rubocop

--- lib/neo4j/core/cypher_session/responses/http.rb
+++ lib/neo4j/core/cypher_session/responses/http.rb
@@ -26,23 +26,41 @@ module Neo4j
               rest_datum = rest_data[i]
 
               case
-              when rest_datum[:labels] # node
-                ::Neo4j::Core::Node.new(rest_datum[:metadata][:id], rest_datum[:metadata][:labels], rest_datum[:data])
-              when rest_datum[:type] # relationship
-                ::Neo4j::Core::Relationship.new(rest_datum[:metadata][:id], rest_datum[:metadata][:type], rest_datum[:data])
+              when rest_datum[:labels]
+                resolve_node(rest_datum)
+              when rest_datum[:type]
+                resolve_relationship(rest_datum)
               when rest_datum[:directions]
-                nodes = rest_datum[:nodes].each_with_index.map do |url, i|
-                  Node.from_url(url, row_datum[2 * i])
-                end
-                relationships = rest_datum[:relationships].each_with_index.map do |url, i|
-                  Relationship.from_url(url, row_datum[(2 * i) + 1])
-                end
-
-                ::Neo4j::Core::Path.new(nodes, relationships, rest_datum[:directions])
+                resolve_path(row_datum, rest_datum)
               else
                 fail "Was not able to determine result entity type: #{rest_data.inspect}"
               end
             end
+          end
+
+          private
+
+          def resolve_node(rest_datum)
+            ::Neo4j::Core::Node.new(rest_datum[:metadata][:id],
+                                    rest_datum[:metadata][:labels],
+                                    rest_datum[:data])
+          end
+
+          def resolve_relationship(rest_datum)
+            ::Neo4j::Core::Relationship.new(rest_datum[:metadata][:id],
+                                            rest_datum[:metadata][:type],
+                                            rest_datum[:data])
+          end
+
+          def resolve_path(row_datum, rest_datum)
+            nodes = rest_datum[:nodes].each_with_index.map do |url, i|
+              Node.from_url(url, row_datum[2 * i])
+            end
+            relationships = rest_datum[:relationships].each_with_index.map do |url, i|
+              Relationship.from_url(url, row_datum[(2 * i) + 1])
+            end
+
+            ::Neo4j::Core::Path.new(nodes, relationships, rest_datum[:directions])
           end
         end
       end
