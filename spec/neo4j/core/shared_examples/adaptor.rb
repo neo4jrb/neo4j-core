bfc7574469c3039de9dbbbf931bf069dbb73bf7f
Brian Underwood <ml+github@semi-sentient.com>
2015-09-30 22:44:51 +1300

    Second fix

--- spec/neo4j/core/shared_examples/adaptor.rb
+++ spec/neo4j/core/shared_examples/adaptor.rb
@@ -35,7 +35,11 @@ RSpec.shared_examples 'Neo4j::Core::CypherSession::Adaptor' do
       end
 
       expect(result[0].to_a[0].n).to be_a(Neo4j::Core::Node)
-      expect(result[0].to_a[0].n.labels).to eq([:Label1])
+      if adaptor.is_a?(::Neo4j::Core::CypherSession::Adaptors::HTTP) && adaptor.version < '2.1.5'
+        expect(result[0].to_a[0].n.labels).to eq(nil)
+      else
+        expect(result[0].to_a[0].n.labels).to eq([:Label1])
+      end
     end
   end
 
