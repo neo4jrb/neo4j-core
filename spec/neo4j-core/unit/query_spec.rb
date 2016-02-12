ff8fc20fd8233601884f658c0bcd83455de12149
Bruce Krysiak <brucek@alum.mit.edu>
2016-02-12 00:42:55 -0800

    Making tests a little better

--- spec/neo4j-core/unit/query_spec.rb
+++ spec/neo4j-core/unit/query_spec.rb
@@ -640,8 +640,8 @@ describe Neo4j::Core::Query do
       it_generates 'CREATE (q:`Person` {age: {q_Person_age}, height: {q_Person_height}})', q_Person_age: nil, q_Person_height: 70
     end
 
-    describe '.create(q: {:\'Child:Person\' => {age: nil, height: 70}})' do
-      it_generates 'CREATE (q:`Child:Person` {age: {q_Child_Person_age}, height: {q_Child_Person_height}})', q_Child_Person_age: nil, q_Child_Person_height: 70
+    describe '.create(q: {\'Child:Person\': {age: 41, height: 70}})' do
+      it_generates 'CREATE (q:`Child:Person` {age: {q_Child_Person_age}, height: {q_Child_Person_height}})', q_Child_Person_age: 41, q_Child_Person_height: 70
     end
   end
 
