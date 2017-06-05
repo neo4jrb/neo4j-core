2a1007977756469c0bcc24ec489a4c2dfd8da297
Jamie Gaskins <jgaskins@gmail.com>
2017-06-04 22:00:45 -0400

    Add support for querying non-integer ranges (#295)
    * Add support for querying non-integer ranges
    
    Passing a time or date range wasn't working if you serialize them as
    non-integer values (I use strings for readability) for two reasons:
    
    - Neo4j expected numeric values for the `RANGE` function
    - Ruby's Range#max doesn't work for non-integer values when the range
      excludes the high end
    
    This commit expands support for ranges to include non-numeric ranges and
    non-integer exclusive ranges.
    
    * Extract range_key_value_string method
    
    * Oops, forgot to extract this variable, too
    
    (cherry picked from commit e74e019b84af89a1a0f28ad56a266b2b882f7ab1)

--- lib/neo4j-core/query_clauses.rb
+++ lib/neo4j-core/query_clauses.rb
@@ -196,9 +196,7 @@ module Neo4j
           self.class.paramaterize_key!(param)
 
           if value.is_a?(Range)
-            min_param, max_param = add_params("#{param}_range_min" => value.min, "#{param}_range_max" => value.max)
-
-            "#{key} IN RANGE({#{min_param}}, {#{max_param}})"
+            range_key_value_string(key, value, previous_keys, param)
           else
             value = value.first if array_value?(value, is_set) && value.size == 1
             operator = array_value?(value, is_set) ? 'IN' : '='
@@ -206,6 +204,17 @@ module Neo4j
             param = add_param(param, value)
 
             "#{key} #{operator} {#{param}}"
+          end
+        end
+
+        def range_key_value_string(key, value, previous_keys, param)
+          case value.begin
+          when Integer
+            min_param, max_param = add_params("#{param}_range_min" => value.min, "#{param}_range_max" => value.max)
+            "#{key} IN RANGE({#{min_param}}, {#{max_param}})"
+          else
+            min_param, max_param = add_params("#{param}_range_min" => value.begin, "#{param}_range_max" => value.end)
+            "#{key} >= {#{min_param}} AND #{previous_keys[-2]}.#{key} <#{'=' unless value.exclude_end?} {#{max_param}}"
           end
         end
 
@@ -281,7 +290,6 @@ module Neo4j
           when Hash then hash_key_value_string(key, value, previous_keys)
           when NilClass then "#{key} IS NULL"
           when Regexp then regexp_key_value_string(key, value, previous_keys)
-          when Array, Range then key_value_string(key, value, previous_keys)
           else
             key_value_string(key, value, previous_keys)
           end
