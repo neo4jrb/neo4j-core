ef61932de9a5bbfa660f2efbbee7173440f81da6
Heinrich Klobuczek <heinrich@mail.com>
2018-12-19 20:33:14 +0100

    rubocop

--- lib/neo4j/core/cypher_session/transactions/driver.rb
+++ lib/neo4j/core/cypher_session/transactions/driver.rb
@@ -8,17 +8,15 @@ module Neo4j
           attr_reader :driver_tx, :driver_session
 
           def initialize(*args)
-            begin
-              super
-              return unless root?
-              @driver_session = session.adaptor.driver.session(Neo4j::Driver::AccessMode::WRITE)
-              @driver_tx = @driver_session.begin_transaction
-            rescue => e
-              clean_transaction_registry
-              @driver_tx.close if @driver_tx
-              @driver_session.close if @driver_session
-              raise e
-            end
+            super
+            return unless root?
+            @driver_session = session.adaptor.driver.session(Neo4j::Driver::AccessMode::WRITE)
+            @driver_tx = @driver_session.begin_transaction
+          rescue StandardError => e
+            clean_transaction_registry
+            @driver_tx.close if @driver_tx
+            @driver_session.close if @driver_session
+            raise e
           end
 
           def commit
